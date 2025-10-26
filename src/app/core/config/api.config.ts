c6217be26e5c9503c282ed72dd58b086df0e63bf
Jeyner17 <jeyner.oswaldo@gmail.com>
2025-10-26 13:18:01 -0500

    config: Actualizar URL del backend para Railway

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,5 +1,14 @@
-export const API_CONFIG = {
+/*export const API_CONFIG = {
   baseUrl: 'http://127.0.0.1:8000/api',
+  endpoints: {
+    provincias: '/provincias',
+    empleados: '/empleados',
+    empleadosReporte: '/empleados/reporte'
+  }
+};*/
+
+export const API_CONFIG = {
+  baseUrl: 'https://gestionempleados-backend-production.up.railway.app/api',
   endpoints: {
     provincias: '/provincias',
     empleados: '/empleados',
